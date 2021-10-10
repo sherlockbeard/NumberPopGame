bb2ac241602c9b4c783af1257d1b9ad1998355b2
sidddharth Kumar <>
2021-10-10 22:13:42 +0530

    added main layout

--- app/src/main/java/com/example/numberpopgame/MainActivity.kt
+++ app/src/main/java/com/example/numberpopgame/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.numberpopgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.numberpopgame.databinding.ActivityMainBinding
 
+
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        binding.StartButton.setOnClickListener {  }
+        setContentView(view)
     }
 }
