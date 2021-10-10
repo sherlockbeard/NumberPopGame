7d5ec05002d0556c6e4006287dbe86cc5f1da4c8
sidddharth Kumar <>
2021-10-10 22:52:17 +0530

    added and contencted layout

--- app/src/main/java/com/example/numberpopgame/GamePage.kt
+++ app/src/main/java/com/example/numberpopgame/GamePage.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class GamePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_page)
+        setContentView(R.layout.game_page)
     }
 }
--- app/src/main/java/com/example/numberpopgame/MainActivity.kt
+++ app/src/main/java/com/example/numberpopgame/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.numberpopgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.numberpopgame.databinding.ActivityMainBinding
 
 
 private lateinit var binding: ActivityMainBinding
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
 
-        binding.StartButton.setOnClickListener {  }
+        binding.StartButton.setOnClickListener {
+            val intent = Intent(this, GamePage::class.java)
+            startActivity(intent)
+        }
         setContentView(view)
     }
 }
